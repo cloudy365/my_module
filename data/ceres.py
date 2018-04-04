2383f35cd0caf014a98c1b14fd6f359b40bb0a39
Yizhe <smzyiz@gmail.com>
2018-04-04 16:58:49 -0500

    add data/comm.py and data/ceres.py

---
+++ data/ceres.py
@@ -0,0 +1,86 @@
+
+# -*- coding: utf-8 -*-
+
+
+__all__ = [
+		"CERES_LV3_grid",
+		"CERES_LV3_Arctic_mean",
+		]
+
+
+from .. import np, Dataset
+from .. import ceres_dir
+
+
+
+def CERES_LV3_grid(dset, fld_name, year, month, version=4.0):
+    """
+    Inputs: dset chose from 'ebaf_toa', 'ebaf_sfc', 'ssf1deg';
+    		fld_name;
+    		year;
+    		month;
+    		version (default is 4.0)
+    Output: chosen data field, please note the dimension could be different.
+    """
+
+    if dset == 'ebaf_toa':
+        if version == 4.0:
+            ncfile = ceres_dir + "/EBAF/CERES_EBAF-TOA_Ed4.0_Subset_200003-201707.nc"
+        elif version == 2.8:
+            ncfile = ceres_dir + "/EBAF/CERES_EBAF-TOA_Ed2.8_Subset_200003-201702.nc"
+    elif dset == 'ebaf_sfc':
+        if version == 4.0:
+            ncfile = ceres_dir + "/EBAF/CERES_EBAF-Surface_Ed4.0_Subset_200003-201701.nc"
+        elif version == 2.8:
+            ncfile = ceres_dir + "/EBAF/CERES_EBAF-Surface_Ed2.8_Subset_200003-201702.nc"
+    elif dset == 'ssf1deg':
+        if year == 2017:
+            ncfile = ceres_dir + "/SSF1deg/monthly/CERES_SSF1deg-Month_Terra-MODIS_Ed4A_Subset_201703-201708.nc"
+        else:
+            ncfile = ceres_dir + "/SSF1deg/monthly/CERES_SSF1deg-Month_Terra-MODIS_Ed4A_Subset_{0}03-{0}12.nc".format(year)
+    else:
+        print "Wrong dataset ..."
+        return
+    
+    ncdf = Dataset(ncfile)
+    fld = ncdf.variables[fld_name][:]
+    
+    if dset == 'ssf1deg':
+        idx = month-3
+    else:
+        idx = (year-2000)*12 + (month-3)
+        
+    if (idx < 0) or (idx >= len(fld)): 
+        print "Your selected time <{}.{}> is out of the time boundary, please choose a time within {}..."\
+                    .format(year, month, ncfile.split('_')[-1][:-3])
+        out_data = np.nan
+    else:
+        out_data = fld[idx]
+    
+    return out_data
+
+
+def CERES_LV3_Arctic_mean(dset, fld_name, year, month, ocean_mask, version=4.0):
+    """
+    Inputs: dset chose from 'ebaf_toa', 'ebaf_sfc', 'ssf1deg';
+    		fld_name;
+    		year;
+    		month;
+    		ocean_mask (True or False);
+    		version (default is 4.0)
+    Output: chosen data field, please note the dimension could be different.
+    """
+
+    m = arc_ocean_mask('CERES')
+    
+    lat = np.arange(70, 90) + 0.5
+    fld = CERES_LV3_grid(dset, fld_name, year, month, version)[-20:, :]
+    
+    if ocean_mask:
+        fld_ocean = np.ma.masked_array(fld, mask=m.mask)
+    else:
+        fld_ocean = fld
+
+    arc_mean = area_weighted_mean(fld_ocean, lat)
+
+    return arc_mean
---
+++ data/comm.py
@@ -0,0 +1,112 @@
+
+# -*- coding: utf-8 -*-
+
+
+__all__ = [
+		"arc_ocean_mask",
+		"area_weighted_mean",
+		"save_data_hdf5",
+		]
+
+
+from .. import np, h5py
+from .. import ocean_mask_1d, ocean_mask_hd, ocean_mask_qd
+
+
+
+
+
+
+def arc_ocean_mask(instrument):
+    """
+    Input: "CERES" or "MISR" or "MERRA"
+    Get Arctic Ocean mask based on ftp://daac.ornl.gov/data/islscp_ii/ancillary/combined_ancillary_xdeg/data/
+    Return the arctic ocean mask (20 * 360) array. Mask out the land grids.
+    """
+
+    raw_data = np.loadtxt(ocean_mask_1d)
+    
+    if instrument == 'CERES':
+        # select Arctic part
+        # because CERES ranges from 70.5 -> 89.5 N (20 points)
+        arctic_0 = raw_data[1:21, :]
+        # transform to CERES format
+        # transform latitude, as CERES lat 70 -> 90 N
+        arctic_1 = arctic_0[::-1]
+        # transform longitude, as CERES lon 0.5 -> 359.5 E
+        arctic_2 = np.array([np.append(arctic_1[i, 180:], arctic_1[i, :180]) for i in range(20)])
+        m = np.ma.masked_equal(arctic_2, 1)
+        
+    elif instrument == 'MISR':
+        # select Arctic part
+        # because CERES ranges from 70.5 -> 89.5 N (20 points)
+        arctic_0 = raw_data[1:21, :]
+        # transform to MISR format
+        # transform latitude, as MISR lat 70 -> 90 N
+        arctic_1 = arctic_0[::-1]
+        m = np.ma.masked_equal(arctic_1, 1)
+        
+    elif instrument == 'MERRA': # same as ERA-Interim
+        arctic_0 = raw_data[:21, :]
+        # keep it
+        m = np.ma.masked_equal(arctic_0, 1)
+        
+    elif instrument == 'CFSR':
+        raw_data = np.loadtxt(cean_mask_hd)
+        # select Arctic part
+        arctic_0 = raw_data[:41, :]     
+        # transform longitude to 0 -> 359.5 E
+        arctic_1 = np.array([np.append(arctic_0[i, 360:], arctic_0[i, :360]) for i in range(41)])
+        m = np.ma.masked_equal(arctic_1, 1)
+    
+    elif instrument == 'JRA55':
+        # convert 0.25 deg ocean mask to 1.25 deg
+        raw_data = np.loadtxt(ocean_mask_qd)
+        # select Arctic part
+        arctic_0 = raw_data[:86, :]
+        
+        tmp_mask = [[] for i in range(17)]    
+        for ilat in range(17):
+            for ilon in range(288):
+                tmp = arctic_0[ilat*5:ilat*5+5, ilon*5:ilon*5+5]
+                if any(tmp.ravel() == 1):
+                    tmp_mask[ilat].append(1)
+                else:
+                    tmp_mask[ilat].append(0)   
+        arctic_0 = np.array(tmp_mask)
+        # transform longitude to 0 -> 359.5 E
+        arctic_1 = np.array([np.append(arctic_0[i, 144:], arctic_0[i, :144]) for i in range(17)])
+        m = np.ma.masked_equal(arctic_1, 1)
+        
+    return m
+
+
+def area_weighted_mean(data, lat):
+    """
+    Function used to calculate area-weighted Arctic mean value. 
+    Write this function because different datasets may have different shapes. 
+    This function then serves as an universal function.
+    Data should be a 2-D array while lat should be a 1-D array, 
+    and they should share the same first dimension.
+    """
+
+    lat_mean_data = np.nanmean(data, axis=1)
+    lat_new = []
+    for ilat, idata in zip(lat, lat_mean_data):
+        if np.isnan(idata) == False:
+            lat_new.append(ilat)
+        else:
+            lat_new.append(np.nan)
+    area_mean = np.nansum(lat_mean_data*np.cos(np.deg2rad(lat))) / np.nansum(np.cos(np.deg2rad(lat)))
+    
+    return area_mean
+
+
+def save_data_hdf5(filename, data_path, data):
+	"""
+	Inputs are filename, data_path, and data
+	"""
+
+    with h5py.File(filename, 'a') as h5f:
+	h5f.create_dataset(data_path, data=data, compression='gzip', compression_opt=5)
+    return
